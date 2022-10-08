0e610fbb44caee61640514c797763f61112b20d0
Robert Mattei <bob@mwbi.net>
2022-10-08 08:58:19 -0400

    Added a root route to: accounts Changed styling of buttons in Nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :entries, only: [:index, :new, :create, :edit,  :update, :destroy]
   resources :categories
   resources :accounts
+  root to:'accounts#index'
   get "/reporting", to: 'entries#reporting'
   mount PgHero::Engine, at: "pghero"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
