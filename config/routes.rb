1a8c05a55cf71fbc87b900fedf021a56dfd64609
Robert Mattei <bob@mwbi.net>
2022-10-10 11:09:32 -0400

    Change routes to get rid of show category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :entries, only: [:index, :new, :create, :edit,  :update, :destroy]
-  resources :categories
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :accounts
   root to:'accounts#index'
   get "/reporting", to: 'entries#reporting'
